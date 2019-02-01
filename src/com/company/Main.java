38d14044c66df21a1f22db97d154a656e1ec5adc
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2019-02-01 08:39:45 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] Arr = {1,5,4,3,7,2,10};
+	MergeSo
+    }
+}
---
+++ src/com/company/merge.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class merge {
+
+    public void MergeSort(int[] arr)
+    {
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr, 0, n-1, temp);
+    }
+
+    public void mergeSortHelper(int[] arr, int left, int right, int[] temp)
+    {
+        if(left < right)
+        {
+            int mid = (left+right)/2;
+            mergeSortHelper(arr, left, mid, temp);
+            mergeSortHelper(arr, mid+1, right, temp);
+            merge(arr, left, mid, right, temp);
+        }
+
+    }
+
+    public void merge(int[]arr, int left, int mid, int right, int[] temp)
+    {
+               
+    }
+}
